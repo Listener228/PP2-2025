c7c535498bc8c8ca2d7ad5b8a20d0007a3c132c0
Bexultan <beksultanshokey2006@gmail.com>
2025-01-18 19:22:13 +0500

    Homework

---
+++ Lab1(Homework)/13.py
@@ -0,0 +1,16 @@
+print("Hello")
+print('Hello')
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = "Hello, World!"
+print(a[1])
+
+b = "Hello, World!"
+print(len(b))
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
