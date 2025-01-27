9519941bae0664bc77015f096a3ef7bc198a33b9
Bexultan <beksultanshokey2006@gmail.com>
2025-01-27 17:47:01 +0500

    HW

---
+++ Lab2/4.py
@@ -0,0 +1,7 @@
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist1.insert(2, "watermelon")
+print(thislist1)
